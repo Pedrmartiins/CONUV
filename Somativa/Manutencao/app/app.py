cf3112894f588297a14b8baf4c897ca3a015acc1
Ramos <viniciusaugusto6996@gmail.com>
2025-05-26 21:17:08 -0300

    manutencao

--- Somativa/Manutencao/app/app.py
+++ Somativa/Manutencao/app/app.py
@@ -1,26 +1,26 @@
 from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
-faturas = {}
+manutencoes = {}
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/fatura', methods=['POST'])
+@app.route('/manutencao', methods=['POST'])
 def gerar():
     data = request.get_json()
-    id = str(len(faturas) + 1)
+    id = str(len(manutencoes) + 1)
     valor = 100  # Exemplo fixo, substituir com cálculo real
-    faturas[id] = {**data, "valor": valor}
-    return jsonify({"id": id, "fatura": faturas[id]}), 201
+    manutencoes[id] = {**data, "valor": valor}
+    return jsonify({"id": id, "manutencao": manutencoes[id]}), 201
 
-@app.route('/fatura/<id>', methods=['GET'])
+@app.route('/manutencao/<id>', methods=['GET'])
 def buscar(id):
-    fatura = faturas.get(id)
-    if not fatura:
-        return jsonify({"erro": "Fatura não encontrada"}), 404
-    return jsonify(fatura)
+    manutencao = manutencoes.get(id)
+    if not manutencao:
+        return jsonify({"erro": "manutencao não encontrada"}), 404
+    return jsonify(manutencao)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5004)
+    app.run(host='0.0.0.0', port=5001)
--- Somativa/Manutencao/app/static/script.js
+++ Somativa/Manutencao/app/static/script.js
@@ -1,21 +1,23 @@
-const form = document.getElementById('faturaForm');
+const form = document.getElementById('manutencaoForm');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const consulta_id = document.getElementById('consulta_id').value;
-  const valor = document.getElementById('valor').value;
+  const aeronave_id = document.getElementById('aeronave_id').value;
+  const peca = document.getElementById('peca_id').value;
+  const motivo = document.getElementById('motivo').value;
+  const tipo = document.getElementById('tipo_manutencao').value;
 
-  const response = await fetch('http://localhost:5004/fatura', {
+  const response = await fetch('http://localhost:5001/manutencao', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ consulta_id, valor })
+    body: JSON.stringify({ aeronave_id, peca, motivo, tipo })
   });
 
   const result = await response.json();
-  document.getElementById('resultado').innerText = JSON.stringify(result, null, 2);
+  document.getElementById('resultado').innerText = JSON.stringify(result, null, 4);
 });
 
 async function listarFaturas() {
-  const res = await fetch('http://localhost:5004/fatura');
+  const res = await fetch('http://localhost:5001/manutencao');
   const result = await res.json();
   document.getElementById('resultado').innerText = JSON.stringify(result, null, 2);
 }
