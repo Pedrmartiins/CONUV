8a3e1ab7e42eca2022d8154a680ad19c9b810c89
Ramos <viniciusaugusto6996@gmail.com>
2025-05-26 22:54:58 -0300

    .

---
+++ Somativa/Certificacao/app/anat_app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/consultar', methods=['POST'])
+def consultar():
+    tecnico_id = request.form.get('tecnico_id')
+    try:
+        response = requests.get(f'http://localhost:5002/verificar_credencial/{tecnico_id}')
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004)
